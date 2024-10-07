f11571c485f8b264209aac1e717852d673e8c645
patled <github@lederer.cloud>
2024-10-07 20:20:58 +0200

    Update dependencies and configuration files

--- consumer/vite.config.ts
+++ consumer/vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { federation } from "@module-federation/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { dependencies } from "../package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    federation({
+      name: "consumer",
+      remotes: {
+        remote: {
+          type: "module",
+          name: "producer",
+          entry: "http://localhost:4174/remoteEntry.js",
+          entryGlobalName: "producer",
+          shareScope: "default",
+        },
+      },
+      exposes: {},
+      filename: "remoteEntry.js",
+      shared: {
+        react: {
+          requiredVersion: dependencies.react,
+          singleton: true,
+        },
+      },
+    }),
+    react(),
+  ],
+});
--- producer/vite.config.ts
+++ producer/vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
+import { dependencies } from "./package.json"; // Adjust the path if necessary
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    federation({
+      filename: "remoteEntry.js",
+      name: "producer",
+      exposes: {
+        "./remote-app": "./src/App.tsx",
+      },
+      remotes: {},
+      shared: {
+        react: {
+          requiredVersion: dependencies.react,
+          singleton: true,
+        },
+      },
+    }),
+    react(),
+  ],
+  server: {
+    port: 4174,
+  },
+  build: {
+    outDir: "dist",
+  },
+  esbuild: {
+    supported: {
+      "top-level-await": true, //browsers can handle top-level-await features
+    },
+  },
+});
